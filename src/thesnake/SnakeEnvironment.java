ce19d8c0dfcd057a461f38ecbbd31df8b37af058
KrishIsBaws <Achyuth1998@terry-pc.brentwood.bc.ca>
2014-01-31 09:02:32 -0800

    fix something

--- src/thesnake/SnakeEnvironment.java
+++ src/thesnake/SnakeEnvironment.java
@@ -8,9 +8,11 @@ import audio.AudioPlayer;
 import environment.Environment;
 import environment.GraphicsPalette;
 import environment.Grid;
+import image.ResourceTools;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.Point;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
@@ -23,6 +25,7 @@ import javax.swing.JOptionPane;
  */
 class SnakeEnvironment extends Environment {
 
+    private GameState state = GameState.PAUSED;
     private Grid grid;
     private int score = 0;
     private Snake snake;
@@ -31,20 +34,20 @@ class SnakeEnvironment extends Environment {
     private ArrayList<Point> apples;
     private ArrayList<Point> poisonapple;
     private ArrayList<Point> diamond;
+    private ArrayList<Point> lightning;
+    private boolean drawPicture = false;
+    private Image exorcist;
 
     public SnakeEnvironment() {
         AudioPlayer.play("/Sound/ObstacleCourse.wav");
-        AudioPlayer.play("/Sound/ObstacleCourse.wav");
-        AudioPlayer.play("/Sound/ObstacleCourse.wav");
-        AudioPlayer.play("/Sound/ObstacleCourse.wav");
-        AudioPlayer.play("/Sound/ObstacleCourse.wav");
-
-        
-
     }
 
     @Override
     public void initializeEnvironment() {
+        this.exorcist = ResourceTools.loadImageFromResource("resources/exorcist2.jpg");
+
+//        this.lightning = ResourceTools.loadImageFromResource("resources/the fist of difference.jpg");
+
         this.setGrid(new Grid());
         this.getGrid().setColor(Color.blue);
         this.getGrid().setColumns(40);
@@ -58,48 +61,22 @@ class SnakeEnvironment extends Environment {
         this.getSnake().getBody().add(new Point(5, 8));
         this.getSnake().getBody().add(new Point(6, 8));
 
-
-
-
         this.apples = new ArrayList<Point>();
-        this.apples.add(getRandomGridLocation());
-        this.apples.add(getRandomGridLocation());
-        this.apples.add(getRandomGridLocation());
-        this.apples.add(getRandomGridLocation());
-        this.apples.add(getRandomGridLocation());
-        this.apples.add(getRandomGridLocation());
-        this.apples.add(getRandomGridLocation());
-        this.apples.add(getRandomGridLocation());
-        this.apples.add(getRandomGridLocation());
-        this.apples.add(getRandomGridLocation());
-        this.apples.add(getRandomGridLocation());
-        this.apples.add(getRandomGridLocation());
-       
-
+        for (int i = 0; i < 13; i++) {
+            this.apples.add(getRandomGridLocation());
+        }
 
         this.poisonapple = new ArrayList<Point>();
-        this.poisonapple.add(getRandomGridLocation());
-        this.poisonapple.add(getRandomGridLocation());
-        this.poisonapple.add(getRandomGridLocation());
-        this.poisonapple.add(getRandomGridLocation());
-        this.poisonapple.add(getRandomGridLocation());
-        this.poisonapple.add(getRandomGridLocation());
-        this.poisonapple.add(getRandomGridLocation());
-        this.poisonapple.add(getRandomGridLocation());
-        this.poisonapple.add(getRandomGridLocation());
-        this.poisonapple.add(getRandomGridLocation());
-        this.poisonapple.add(getRandomGridLocation());
-        this.poisonapple.add(getRandomGridLocation());
-        
-        
-        
-        
-
-
+        for (int i = 0; i < 7; i++) {
+            this.poisonapple.add(getRandomGridLocation());
+        }
 
 
         this.diamond = new ArrayList<Point>();
         this.diamond.add(getRandomGridLocation());
+
+        this.lightning = new ArrayList<Point>();
+        this.lightning.add(getRandomGridLocation());
 
     }
 
@@ -111,34 +88,36 @@ class SnakeEnvironment extends Environment {
     @Override
     public void timerTaskHandler() {
 //        System.out.println("use your keys");
-        if (snake != null) {
-            if (moveCounter <= 0) {
-                snake.move();
-                moveCounter = speed;
-                speed = 0;
-                checkSnakeIntersection();
-            } else {
-                moveCounter--;
-            }
+        if (getState() == GameState.RUNNING) {
+            if (snake != null) {
+                if (moveCounter <= 0) {
+                    snake.move();
+                    moveCounter = speed;
+                    speed = 0;
+                    checkSnakeIntersection();
+                } else {
+                    moveCounter--;
+                }
 
-            if (snake.getDirection() == Direction.RIGHT) {
-                if (snake.getHead().x >= this.grid.getColumns()) {
-                    snake.getHead().x = 0;
+                if (snake.getDirection() == Direction.RIGHT) {
+                    if (snake.getHead().x >= this.grid.getColumns()) {
+                        snake.getHead().x = 0;
+                    }
+                } else if (snake.getDirection() == Direction.LEFT) {
+                    if (snake.getHead().x <= -1) {
+                        snake.getHead().x = this.grid.getColumns() - 1;
+                    }
+                } else if (snake.getDirection() == Direction.UP) {
+                    if (snake.getHead().y <= -1) {
+                        snake.getHead().y = this.grid.getRows() - 1;
+                    }
+                } else if (snake.getDirection() == Direction.DOWN) {
+                    if (snake.getHead().y >= this.grid.getRows()) {
+                        snake.getHead().y = 0;
+                    }
                 }
-            } else if (snake.getDirection() == Direction.LEFT) {
-                if (snake.getHead().x <= -1) {
-                    snake.getHead().x = this.grid.getColumns() - 1;
-                }
-            } else if (snake.getDirection() == Direction.UP) {
-                if (snake.getHead().y <= -1) {
-                    snake.getHead().y = this.grid.getRows() - 1;
-                }
-            } else if (snake.getDirection() == Direction.DOWN) {
-                if (snake.getHead().y >= this.grid.getRows()) {
-                    snake.getHead().y = 0;
-                }
-            }
 
+            }
         }
 
     }
@@ -155,17 +134,25 @@ class SnakeEnvironment extends Environment {
                 System.out.println("APPLE CHOMP");
                 AudioPlayer.play("/Sound/crunching-1.wav");
                 this.snake.grow(1);
-                this.score += 1;
+                this.setScore(this.getScore() + 1);
                 this.speed -= 2;
                 this.apples.get(i).x = (int) (Math.random() * this.grid.getColumns());
                 this.apples.get(i).y = (int) (Math.random() * this.grid.getRows());
             }
         }
-
+        for (int i = 0; i < this.lightning.size(); i++) {
+            if (snake.getHead().equals(this.lightning.get(i))) {
+                System.out.println("FASTER");
+                AudioPlayer.play("/Sound/chime.wav");
+                this.speed -= 30;
+                this.lightning.get(i).x = (int) (Math.random() * this.grid.getColumns());
+                this.lightning.get(i).y = (int) (Math.random() * this.grid.getRows());
+            }
+        }
         for (int i = 0; i < this.poisonapple.size(); i++) {
             if (snake.getHead().equals(this.poisonapple.get(i))) {
                 System.out.println("BOOOOOM");
-                this.score -= 5;        
+                this.setScore(this.getScore() - 5);
                 AudioPlayer.play("/Sound/bomb.wav");
                 JOptionPane.showMessageDialog(null, "BOOM!! Great job!!");
                 this.poisonapple.get(i).x = (int) (Math.random() * this.grid.getColumns());
@@ -176,9 +163,9 @@ class SnakeEnvironment extends Environment {
             if (snake.getHead().equals(this.diamond.get(i))) {
                 System.out.println("OOHH DIAMONDS");
                 this.snake.grow(2);
-                this.score += 10;
+                this.setScore(this.getScore() + 10);
                 this.speed -= 2;
-                AudioPlayer.play("/Sound/magic-chime-02.wav");
+                AudioPlayer.play("/Sound/chime.wav");
 //                JOptionPane.showMessageDialog(null, "OOHH");
                 this.diamond.get(i).x = (int) (Math.random() * this.grid.getColumns());
                 this.diamond.get(i).y = (int) (Math.random() * this.grid.getRows());
@@ -198,8 +185,16 @@ class SnakeEnvironment extends Environment {
 
     @Override
     public void keyPressedHandler(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_P) {
+            if (state == GameState.RUNNING) {
+               state = GameState.PAUSED;
+            } else if (state == GameState.PAUSED) {
+                state  = GameState.RUNNING;
+            }
+        }
+        
         if (e.getKeyCode() == KeyEvent.VK_SPACE) {
-            this.score += 10;
+            this.setScore(this.getScore() + 10);
         } else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
             getSnake().setDirection(Direction.RIGHT);
             getSnake().move();
@@ -212,6 +207,8 @@ class SnakeEnvironment extends Environment {
         } else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
             getSnake().setDirection(Direction.DOWN);
             getSnake().move();
+        } else if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+           state = GameState.ENDED;
         }
     }
 
@@ -242,7 +239,12 @@ class SnakeEnvironment extends Environment {
             }
             if (this.diamond != null) {
                 for (int i = 0; i < this.diamond.size(); i++) {
-                    GraphicsPalette.drawDiamond(graphics, this.grid.getCellPosition(this.diamond.get(i)), new Point(this.grid.getCellSize()), Color.BLUE);
+                    GraphicsPalette.drawDiamond(graphics, this.grid.getCellPosition(this.diamond.get(i)), this.grid.getCellSize(), Color.BLUE);
+
+                }
+            }
+            if (this.lightning != null) {
+                for (int i = 0; i < this.lightning.size(); i++) {
                 }
             }
 
@@ -252,25 +254,34 @@ class SnakeEnvironment extends Environment {
                 for (int i = 0; i < getSnake().getBody().size(); i++) {
                     cellLocation = getGrid().getCellPosition(getSnake().getBody().get(i));
                     graphics.fillRect(cellLocation.x, cellLocation.y, getGrid().getCellWidth(), getGrid().getCellHeight());
-
-
                 }
 
             }
         }
 
 
-        GraphicsPalette.enterPortal(graphics, new Point(500, 100), new Point(this.grid.getCellSize()), Color.yellow);
-        GraphicsPalette.leavePortal(graphics, new Point(200, 200), new Point(this.grid.getCellSize()), Color.yellow);
+        GraphicsPalette.enterPortal(graphics, new Point(300, 50), new Point(this.grid.getCellSize()), Color.yellow);
+        GraphicsPalette.leavePortal(graphics, new Point(650, 50), new Point(this.grid.getCellSize()), Color.yellow);
 
 
 
         graphics.setColor(Color.BLUE);
-        graphics.setFont(new Font("Calibri", Font.ITALIC, 50));
-        graphics.drawString("Score: " + this.score, 50, 50);
+        graphics.setFont(new Font("Mistral", Font.ITALIC, 50));
+        graphics.drawString("Score: " + this.getScore(), 125, 75);
 
-        graphics.setFont(new Font("Comic Sans MS", Font.BOLD, 72));
-        graphics.drawString("Snake Run ", 270, 75);
+        graphics.setFont(new Font("Mistral", Font.BOLD, 72));
+        graphics.drawString("Snake Run ", 370, 75);
+
+        if (drawPicture) {
+            graphics.drawImage(exorcist, 150, 0, this);
+        }
+        if (state == GameState.ENDED) {
+            graphics.setFont(new Font("Calibri", Font.ITALIC, 100));
+            graphics.drawString("GAME OVER!!", 200, 300);
+        }
+
+
+
     }
 
     /**
@@ -299,5 +310,41 @@ class SnakeEnvironment extends Environment {
      */
     public void setGrid(Grid grid) {
         this.grid = grid;
+    }
+
+    /**
+     * @return the state
+     */
+    public GameState getState() {
+        return state;
+    }
+
+    /**
+     * @param state the state to set
+     */
+    public void setState(GameState state) {
+        this.state = state;
+    }
+
+    /**
+     * @return the score
+     */
+    public int getScore() {
+        return score;
+    }
+
+    /**
+     * @param score the score to set
+     */
+    public void setScore(int score) {
+        if ((this.score < 60) && (score >= 60)) {
+            System.out.println("WOOOOOOOOOOT");
+            //show picture
+            // play sound
+            AudioPlayer.play("/Sound/screaming.wav");
+            setState(GameState.ENDED);
+            this.drawPicture = true;
+        }
+        this.score = score;
     }
 }
--- src/thesnake/TheSnake.java
+++ src/thesnake/TheSnake.java
@@ -24,21 +24,20 @@ public class TheSnake {
       
         start();
         
-        saySomething("asda");
     }
 
     private static void start() {
-        Environment myGame = new SnakeEnvironment();
+        SnakeEnvironment myGame = new SnakeEnvironment();
         myGame.setBackground(ResourceTools.loadImageFromResource("resources/Yellow.jpg"));
         
         ApplicationStarter.run("TheSnakeGame", myGame);
+        saySomething("asda");
+        myGame.setState(GameState.RUNNING);
     }
 
     private static void saySomething(String asda) {
           JOptionPane.showMessageDialog(null, "Welcome to snake run");
-          JOptionPane.showMessageDialog(null, "To Play this game, you must use the arrow keys to navigate where the snake goes...");
-          JOptionPane.showMessageDialog(null, "Try to eat the apples and diamonds while avoiding the Bombs or you will get an annoying message..");
-          JOptionPane.showMessageDialog(null, "The aim is to get the highest score possible without using the SECERET cheat code");
-          JOptionPane.showMessageDialog(null, "Good Luck!");
+          JOptionPane.showMessageDialog(null, "To Play this game, you must use the arrow keys to navigate where the snake goes. Try to eat the apples and diamonds while avoiding the Bombs. The aim is to get 60 points. GL!");
+
         }
 }
